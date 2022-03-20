a13d9fb186f153c0ad7ea92e09c0dc54efb7c688
Matias Anriquez <matiasanriquez@hotmail.com>
2022-03-20 20:50:56 -0300

    desafio 5

---
+++ src/main/java/pages/CartPage.java
@@ -0,0 +1,49 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class CartPage extends IndexPage {
+
+    public static WebDriverWait wait;
+    @FindBy(xpath = "/html/body/div[6]/div/div[1]/div/table/tbody/tr/td[2]")
+    WebElement cartTittleProduct;
+
+    @FindBy(xpath ="/html/body/div[6]/div/div[1]/div/table/tbody/tr/td[3]" )
+    WebElement cartPriceProduct;
+
+    @FindBy(xpath = "//button[contains(text(),'Place Order')]")
+    WebElement placeOrderButton;
+
+    public CartPage() {
+        this.driver = getDriver();
+        PageFactory.initElements(driver, this);
+        this.wait = utility.DriverFactory.getWait();
+    }
+
+    public String getTittle() {
+        return getTextFromWebElement(cartTittleProduct);
+    }
+    //chequear como traer solo el monto precio--ProductoInfoPage
+    public String getPrice() {
+        return "$" + getTextFromWebElement(cartPriceProduct)  + " *includes tax";
+    }
+
+    public void clickOrder() {
+        clickOnWebElement(placeOrderButton);
+    }
+    /*
+    public void clickOrder() {
+        wait.until(ExpectedConditions.elementToBeClickable(placeOrderButton));
+        clickOnWebElement(placeOrderButton);
+    }
+    */
+
+}
+
---
+++ src/main/java/pages/FormPage.java
@@ -0,0 +1,63 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FormPage extends IndexPage{
+
+    @FindBy(id = "name")
+    WebElement formName;
+
+    @FindBy(id = "country")
+    WebElement formCountry;
+
+    @FindBy(id = "city")
+    WebElement formCity;
+
+    @FindBy(id = "card")
+    WebElement formCard;
+
+    @FindBy(id = "month")
+    WebElement formMonth;
+
+    @FindBy(id = "year")
+    WebElement formYear;
+
+    @FindBy(xpath = "/html/body/div[3]/div/div/div[3]/button[2]")
+    WebElement formPurchase;
+
+    public void FormPage(){
+        this.driver = getDriver();
+        PageFactory.initElements(driver, this);
+    }
+
+    public void inputName(String name){
+        sendKeysToWebElement(formName, name);
+    }
+
+    public void inputCountry(String country) {
+        sendKeysToWebElement(formCountry, country);
+    }
+
+    public void inputCity(String city) {
+        sendKeysToWebElement(formCity, city);
+    }
+
+    public void inputCard(String card) {
+        sendKeysToWebElement(formCard, card);
+    }
+
+    public void inputMonth(String month) {
+        sendKeysToWebElement(formMonth, month);
+    }
+
+    public void inputYear(String year) {
+        sendKeysToWebElement(formYear, year);
+    }
+
+    public void clickPurchase() {
+        clickOnWebElement(formPurchase);
+    }
+
+}
